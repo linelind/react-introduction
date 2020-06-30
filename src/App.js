c6e72f59a6ac9697f64dd5360e706093e1a54f92
cnnrbrn <connorbobrien@gmail.com>
2020-06-30 21:02:40 +0200

    Add Sass to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./sass/style.scss";
 import Heading from "./components/Heading";
 import Paragraph from "./components/Paragraph";
 import List from "./components/List";
