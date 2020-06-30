8294fd87f51c00a55b6b1cfa6eea75ccadb8100b
cnnrbrn <connorbobrien@gmail.com>
2020-06-30 22:51:04 +0200

    Add sticky footer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import "./sass/style.scss";
 import Heading from "./components/Heading";
 import Paragraph from "./components/Paragraph";
 import List from "./components/List";
+import Footer from "./components/Footer";
 
 function App() {
     return (
-        <div className="container">
-            <Heading />
-            <Paragraph />
-            <List />
-        </div>
+        <>
+            <div className="wrapper">
+                <div className="container">
+                    <Heading />
+                    <Paragraph />
+                    <List />
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
